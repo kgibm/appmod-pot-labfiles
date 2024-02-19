10ac85b3c2e15e39c6a5b0a8f4310cc8baf49931
Kevin Postreich <kevinlp@us.ibm.com>
2024-02-19 16:00:57 -0500

    update lab setup sh

--- labs/RuntimeModernization/scripts/lab-setup.sh
+++ labs/RuntimeModernization/scripts/lab-setup.sh
@@ -3,8 +3,6 @@
 ######################
 
 ## No input parameters required to run this script. 
-#If the Git repo dir does not exist, clone the repo and set permissions on scripts. 
-## otherwise, just set permissions on scripts, just in case. 
 #cleanup student migration bundle if present.
 #unzip completed mig bundle from cloned git repo to student folder
  
@@ -43,21 +41,6 @@ if [  -f "$LOG" ]; then
 fi
 
 
-#clone the got repo for the labs, if it does not exist on the VM
-if [ ! -d "$CLONE_REPO_DIR" ]; then
-    echo "---> Cloning the git repo $GIT_REPO" | tee -a $LOG
-    git clone $GIT_REPO
-fi    
-# git repo dir must exist, ensure scripts are executable
-echo "---> Setting execute permissions on scripts in the lab files repo" | tee -a $LOG
-find ./appmod-pot-labfiles -name "*.sh" -exec chmod +x {} \; 
-echo ""
-echo "-------------------------------------------"
-echo "The cloned git repo is available and ready:" 
-echo " --> $APPMOD_DIR"
-echo "-------------------------------------------"     
-echo ""
-
 echo "---> pbw bundle path: $STUDENT_PBW_BUNDLE"
 
 if [ -d "$STUDENT_PBW_BUNDLE" ]; then
@@ -65,8 +48,13 @@ if [ -d "$STUDENT_PBW_BUNDLE" ]; then
      rm -rf $STUDENT_PBW_BUNDLE ;
      sleep 2
 fi
-
+echo ""
+echo "----------------------------------------------------"
 echo " --> Extracting the pbw bundle from Sudent directory" | tee -a $LOG
+echo ""
+echo "unzip -d $APPMOD_DIR/ $CLONE_REPO_DIR/labs/RuntimeModernization/pbw-bundle-complete.zip" 
+echo "-----------------------------------------------------"
+sleep 4
 
 unzip -d $APPMOD_DIR/ $CLONE_REPO_DIR/labs/RuntimeModernization/pbw-bundle-complete.zip 
 
@@ -81,12 +69,16 @@ echo "rc: $rc"
     exit 1
     echo ""
   fi
+  
+  sleep 3
 
 echo "" | tee -a $LOG 
 echo "--------------------------------------------------------------" | tee -a $LOG
 echo "The Transformatin Advisor Migration Bundle is extracted here:" | tee -a $LOG
 echo "  ---> $STUDENT_PBW_BUNDLE" | tee -a $LOG
 echo "--------------------------------------------------------------"  | tee -a $LOG
+
+sleep 2 
     
 echo "" | tee -a $LOG
 echo "==========================" | tee -a $LOG
